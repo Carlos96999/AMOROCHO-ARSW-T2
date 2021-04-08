158a2e81651d2bcaf0c33e3cb4a39fac7f13b962
Carlos Amorocho <allblacksc@hotmail.com>
2021-04-08 08:03:32 -0500

    Commit inicial

---
+++ src/main/java/edu/escuelaing/co/AMOROCHOARSWT2/AmorochoArswT2Application.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.co.AMOROCHOARSWT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmorochoArswT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmorochoArswT2Application.class, args);
+	}
+
+}
